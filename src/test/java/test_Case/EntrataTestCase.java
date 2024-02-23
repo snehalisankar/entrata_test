57cd4bcae5f02507264a0f7c9be582230ca89b18
aviderkar <aviderkar2013@gmail.com>
2024-02-23 19:42:58 +0530

    changes updated

--- src/test/java/test_Case/EntrataTestCase.java
+++ src/test/java/test_Case/EntrataTestCase.java
@@ -30,66 +30,51 @@ public class EntrataTestCase extends BasePage {
 			 WebDriverWait wait= new WebDriverWait(BasePage.driver,Duration.ofSeconds(20));
 			 System.out.println("before btn");
 			 WebElement watchdemobtn= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@class='button-default solid-dark-button'][normalize-space()='Watch Demo']")));
-			 //  //*[@id="gatsby-focus-wrapper"]/div/div[1]/div/div/div[4]/div[6]/a[2]
-			 
-			
-//			WebElement watchdemobtn= driver.findElement(By.xpath("//*[@id=\"gatsby-focus-wrapper\"]/div/div[1]/div/div/div[3]/a[1]"));
-			
 			watchdemobtn.click();
 			System.out.println("after btn");
-			WebElement firstName= driver.findElement(By.xpath("//input[@id=\"FirstName\"]"));
+			 
+			
+			WebElement firstName= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='FirstName']")));
 			firstName.click();
 			firstName.sendKeys("snehal");
 			
-			WebElement lastName= driver.findElement(By.xpath("//input[@id=\"LastName\"]"));
+			WebElement lastName= driver.findElement(By.xpath("//input[@id='LastName']"));
 			lastName.click();
 			lastName.sendKeys("Isankar");
 			
-			WebElement email= driver.findElement(By.xpath("//input[@id=\"Email\"]"));
+			WebElement email= driver.findElement(By.xpath("//input[@id='Email']"));
 			email.click();
 			email.sendKeys("snehal11@gmail.com");
 			
-			WebElement companyName= driver.findElement(By.xpath("//input[@id=\"Company\"]"));
+			WebElement companyName= driver.findElement(By.xpath("//input[@id='Company']"));
 			companyName.click();
 			companyName.sendKeys("ACDFHJJ");
 			
-			WebElement phoneNo= driver.findElement(By.xpath("//input[@id=\"Phone\"]"));
+			WebElement phoneNo= driver.findElement(By.xpath("//input[@id='Phone']"));
 			phoneNo.click();
 			phoneNo.sendKeys("8765431212");
 			
 //			WebElement unitcountDropdown= driver.findElement(By.xpath("//select[@id='Unit_Count__c']"));   //use of select class for dropdown
-//			Select s=new Select(unitcountDropdown);
-////			s.selectByValue("1-10");
+//	     	Select s=new Select(unitcountDropdown);
+//     		s.selectByValue("1-10");
 //			s.deselectByIndex(2);
-//			
-//			JavascriptExecutor js=(JavascriptExecutor)driver;
-//		
-//			WebElement jobtitle= driver.findElement(By.xpath("//input[@aria-describedby='ValidMsgTitle']"));
-//			js.executeScript("argument[0].ScrollIntoView()",jobtitle);
-//			js.executeScript ("window.scrollBy(0,200)");
-//			
-//			jobtitle.click();
-//			jobtitle.sendKeys("Test Engineer");
-//			
+			
+			JavascriptExecutor js=(JavascriptExecutor)driver;
+			js.executeScript("window.scrollBy(0,200)");
+		
+		
+			WebElement jobtitle= driver.findElement(By.xpath("//input[@id='Title']"));
+	     	jobtitle.click();
+			jobtitle.sendKeys("Test Engineer");
+			
 			driver.navigate().back();
 			
 			 }
 		 
-
- //TestCase 2-check the resource tab 
 		 
-		 @Test(priority = 2,enabled = true,description = "Check resource tab")
-		    public void resourcevalidation() throws Exception {
-			 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-			WebElement resource= driver.findElement(By.xpath("//div[@class='header-nav-item']//a[@class='main-nav-link'][normalize-space()='Resources']"));
-			resource.click();
-			driver.navigate().back();
-		 }
+ //TestCase 2-check the sign in  
 		 
-		 
- //TestCase 3-check the sign in  
-		 
-		 @Test(priority = 3,enabled = true,description = "Checksign in  ")
+		 @Test(priority = 2,enabled = true,description = "Checksign in  ")
 		    public void SignIn() throws Exception {
 			 
 			 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
@@ -107,22 +92,74 @@ public class EntrataTestCase extends BasePage {
 			WebElement password= driver.findElement(By.xpath("//input[@id='entrata-password']"));
 			password.click();
 			password.sendKeys("ACDFHJJ");
-			driver.navigate().back();
+			driver.navigate().to("https://www.entrata.com/");
+			
 		 }
 		 
-		//TestCase 4-baseCampvalidation test case is validating the base camp tab and then by clicking on "RegisterNow" tab it will fill up the personal information form  by provinding addtional details line above test case and click on "Next" button
-//		 @Test(priority = 4,enabled = true,description = "validating the base camp tab")
-//		    public void baseCampvalidation() throws Exception {
-//			 
-//			WebElement basecamp= driver.findElement(By.xpath("//div[@class='header-nav-item']//a[@class='main-nav-link'][normalize-space()='Base Camp']"));
-//			 basecamp.click();
-//
-//			 WebElement registernow= driver.findElement(By.xpath("//div[@class='text-block-18']"));    
-//			 registernow.click();
-//			 driver.navigate().back();
-//			 
-//		 }
+		
+//TestCase 3-check the resource tab details inside that check guide section and click on download button and fill the next form navigate to main page.
 		 
+		 @Test(priority = 3,enabled = true,description = "Check resource tab")
+		    public void resourcevalidation() throws Exception {
+			 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+			 WebElement resource= driver.findElement(By.xpath("(//div[@class='main-nav-link'])[3]"));   
+			resource.click();
+			
+			WebElement guides= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//a[@class='fat-nav-links'][normalize-space()='Guides'])[1]")));
+			Actions act = new Actions(driver);
+			act.moveToElement(guides).click().perform();
+			
+			WebElement downloadbtn= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='all-content']//div[@class='grid-item first-grid-item']//a[@class='button-default solid-dark-button'][normalize-space()='Download Now']")));
+			Actions actions = new Actions(driver);
+			actions.moveToElement(downloadbtn).click().perform();
+			
+		
+			
+			WebElement firstName= driver.findElement(By.xpath("//input[@id='FirstName']"));
+			firstName.click();
+			firstName.sendKeys("snehal");
+			
+			WebElement lastName= driver.findElement(By.xpath("//input[@id='LastName']"));
+			lastName.click();
+			lastName.sendKeys("Isankar");
+			
+			WebElement email= driver.findElement(By.xpath("//input[@id='Email']"));
+			email.click();
+			email.sendKeys("snehal11@gmail.com");
+			
+			WebElement companyName= driver.findElement(By.xpath("//input[@id='Company']"));
+			companyName.click();
+			companyName.sendKeys("ACDFHJJ");
+			
+			WebElement phoneNo= driver.findElement(By.xpath("//input[@id='Phone']"));
+			phoneNo.click();
+			phoneNo.sendKeys("8765431212");
+			
+//			WebElement unitcountDropdown= driver.findElement(By.xpath("//select[@id='Unit_Count__c']"));   //use of select class for dropdown
+//	     	Select s=new Select(unitcountDropdown);
+//     		s.selectByValue("1-10");
+//			s.deselectByIndex(2);
+			
+		
+			WebElement jobtitle= driver.findElement(By.xpath("//input[@id='Title']"));
+	     	jobtitle.click();
+			jobtitle.sendKeys("Test Engineer");
+		
+			driver.navigate().to("https://www.entrata.com/");
+			
+		 }
+		 
+//TestCase 4-baseCampvalidation test case is validating the base camp tab and then by clicking on "RegisterNow" tab it will fill up the personal information form  by provinding addtional details line above test case and click on "Next" button
+		 @Test(priority = 4,enabled = true,description = "validating the base camp tab")
+	    public void baseCampvalidation() throws Exception {
+			 
+		WebElement basecamp= driver.findElement(By.xpath("//div[@class='header-nav-item']//a[@class='main-nav-link'][normalize-space()='Base Camp']"));
+	 basecamp.click();
+			 		 
+		  driver.navigate().to("https://www.entrata.com/");	
+		  driver.quit();
+			
+	 }
 		 
 		 
 }
